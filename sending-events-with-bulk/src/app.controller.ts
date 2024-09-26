67d25f6c432c442b58bd6ea51b16f0cb903744cd
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-26 19:32:12 -0300

    feat: adding http methods in controller

--- sending-events-with-bulk/src/app.controller.ts
+++ sending-events-with-bulk/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/pontos')
+  findAll() {
+    return this.appService.findAll();
+  }
+
+  @Post('/pontos')
+  addPonto(@Request() req: PontoDTO) {
+    const { employeeId } = req;
+    const date = new Date();
+    const after_date = new Date(date.getTime() + 1000 * 60 * 60 * 8);
+    return this.appService.save({
+      checkin: date,
+      checkout: after_date,
+      employeeId,
+    });
+  }
+
+  @Delete('/pontos')
+  clear() {
+    return this.appService.clear();
   }
 }
+
+export type PontoDTO = {
+  checkin: Date;
+  checkout: Date;
+  employeeId: string;
+};
