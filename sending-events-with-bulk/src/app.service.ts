01fb31be4611f9a5e1e45652d3bd498f0e61947c
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-26 19:30:21 -0300

    feat: adding crud operations in memory

--- sending-events-with-bulk/src/app.service.ts
+++ sending-events-with-bulk/src/app.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly events: Ponto[] = [];
+  async save(ponto: Ponto) {
+    this.events.push(ponto);
+  }
+  async findAll(): Promise<Ponto[]> {
+    return this.events;
+  }
+  async clear() {
+    this.events.length = 0;
   }
 }
+
+export type Ponto = {
+  checkin: Date;
+  checkout: Date;
+  employeeId: string;
+};
