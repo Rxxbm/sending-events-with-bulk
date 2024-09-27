3c9afbe06c68a2caeb5757c610ad18abb3d2cc76
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-26 21:55:24 -0300

    feat: adding swagger

--- sending-events-with-bulk/src/app.controller.ts
+++ sending-events-with-bulk/src/app.controller.ts
@@ -1,24 +1,68 @@
-import { Controller, Delete, Get, Patch, Post, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Param,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  ApiOperation,
+  ApiTags,
+  ApiParam,
+  ApiBody,
+  ApiResponse,
+} from '@nestjs/swagger';
+
+export class PontoDTO {
+  employeeId: string;
+}
 
 @Controller()
+@ApiTags('Pontos Controller')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/pontos/:employeeId')
-  findOne(@Request() req) {
-    const { employeeId } = req.params;
+  @ApiOperation({ summary: 'Find ponto by employee ID' })
+  @ApiParam({
+    name: 'employeeId',
+    required: true,
+    description: 'ID of the employee',
+  })
+  @ApiResponse({ status: 200, description: 'Ponto found' })
+  @ApiResponse({ status: 404, description: 'Ponto not found' })
+  findOne(@Param('employeeId') employeeId: string) {
     return this.appService.findByEmployeeId(employeeId);
   }
 
   @Get('/pontos')
+  @ApiOperation({ summary: 'Return all pontos' })
+  @ApiResponse({ status: 200, description: 'All pontos returned' })
   findAll() {
     return this.appService.findAll();
   }
 
   @Post('/pontos')
-  async addPonto(@Request() req) {
-    const body: PontoDTO = req.body;
+  @ApiOperation({ summary: 'Add a new ponto' })
+  @ApiBody({
+    description: 'Ponto data',
+    type: PontoDTO,
+    examples: {
+      exemplo1: {
+        summary: 'Valid ponto example',
+        description: 'Adding ponto for employee',
+        value: {
+          employeeId: 'any_employee_id',
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Ponto created' })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  async addPonto(@Body() body: PontoDTO) {
     const { employeeId } = body;
     const date = new Date();
     const after_date = null;
@@ -35,17 +79,22 @@ export class AppController {
   }
 
   @Delete('/pontos')
+  @ApiOperation({ summary: 'Clear all pontos' })
+  @ApiResponse({ status: 200, description: 'All pontos cleared' })
   clear() {
     return this.appService.clear();
   }
 
   @Patch('/pontos/:employeeId')
-  close(@Request() req) {
-    const { employeeId } = req.params;
+  @ApiOperation({ summary: 'Close ponto by employee ID' })
+  @ApiParam({
+    name: 'employeeId',
+    required: true,
+    description: 'ID of the employee',
+  })
+  @ApiResponse({ status: 200, description: 'Ponto closed' })
+  @ApiResponse({ status: 404, description: 'Ponto not found' })
+  close(@Param('employeeId') employeeId: string) {
     return this.appService.close(employeeId);
   }
 }
-
-export type PontoDTO = {
-  employeeId: string;
-};
--- sending-events-with-bulk/src/main.ts
+++ sending-events-with-bulk/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Pontos API')
+    .setDescription(
+      'This api is responsible for managing the points of employees and sending them to the email if the employee has not checked out.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
